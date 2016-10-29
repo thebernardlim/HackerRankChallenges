4b9ee65d4de8bfcd29f8a0807ca9a7c2a517b6c9
thebernardlim <ber9000@gmail.com>
2016-10-29 22:46:01 +0800

    Day 4, 5

---
+++ 30 Days of Code/Day 4.cs
@@ -0,0 +1,12 @@
+class Person {
+    public int age;     
+	public Person(int initialAge) {
+        // Add some more code to run some checks on initialAge
+     }
+     public void amIOld() {
+        // Do some computations in here and print out the correct statement to the console 
+     }
+
+     public void yearPasses() {
+        // Increment the age of the person in here
+     }
